1f241302a7a89e0ba723f3fec74ef42a59afdafb
Zari Innis <inniszari@gmail.com>
2025-10-11 08:12:20 -0500

    Edited tic tac toe.js to put in x's and o's

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -3,8 +3,35 @@ window.onload=function(){
 
 const grid=document.querySelectorAll('#board div');
 // selecting the board
-grid.forEach(square=>{
+
+grid.forEach((square)=>{
     square.classList.add('square');// adding the css
+    
 });
 
+
+let player_turn= 0;
+
+grid.forEach((square, index)=>{
+    square.addEventListener('click',function(){
+        if(!square.classList.contains('O')&& !square.classList.contains('X')){
+            if (player_turn%2==0){
+                square.classList.add('X');
+                square.textContent="X";
+                player_turn++;
+            }
+            else{
+                square.classList.add('O');
+                square.textContent="O"
+                player_turn++;
+
+            }
+        const win=[];
+        }
+    });
+});
+
+
+
 };
+ 
